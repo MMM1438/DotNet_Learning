7c6dc5f0cf274e4dcb27fc4f68bc5111a2bb8aad
MMM1438 <nitipon.mmdev@gmail.com>
2025-11-02 20:09:33 +0700

    dotnet learning

---
+++ WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            Student s1 = new Student(1,"Roger",100);
+            Student s2 = new Student(2, "Max", 99);
+            Student s3 = new Student(3, "Jill", 45);
+
+            List<Student> allStudents = new List<Student>();
+            allStudents.Add(s1);
+            allStudents.Add(s2);
+            allStudents.Add(s3);
+
+            return View(allStudents);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Showscore(int id)
+        {
+            return Content($"คะแนนสอบของนักเรียน เลขที่ {id}");
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public Student(int id, string name, int score)
+        {
+            Id = id;
+            Name = name;
+            Score = score;
+        }
+        public Student()
+        {
+            Id = 0;
+            Name = string.Empty;
+            Score = 0;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
