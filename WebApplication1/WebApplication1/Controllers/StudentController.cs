8936783f1b4b3cda20b4d1910365452f63b0ba1e
MMM1438 <nitipon.mmdev@gmail.com>
2025-11-05 21:56:33 +0700

    Add set and get data to database

--- WebApplication1/WebApplication1/Controllers/StudentController.cs
+++ WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -1,26 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly ApplicationDBContext _db;
+        public StudentController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            Student s1 = new Student(1,"Roger",100);
-            Student s2 = new Student(2, "Max", 99);
-            Student s3 = new Student(3, "Jill", 45);
-
-            List<Student> allStudents = new List<Student>();
-            allStudents.Add(s1);
-            allStudents.Add(s2);
-            allStudents.Add(s3);
-
+            IEnumerable <Student> allStudents = _db.Students;
             return View(allStudents);
         }
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Student obj)
+        {
+            _db.Students.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
         public IActionResult Showscore(int id)
         {
